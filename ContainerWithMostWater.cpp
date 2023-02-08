92ff3c5d3fd8c984340a0d064c50bfbff9e3ed32
Yeabsira Yonas <121499744+yabulala432@users.noreply.github.com>
2023-02-09 01:24:29 +0300

    Update ContainerWithMostWater.cpp

--- ContainerWithMostWater.cpp
+++ ContainerWithMostWater.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
         int maxArea = 0;
-        int s = 0, e = height.size()-1;
-
-        while(s < e){
-            if(height[s] <= height[e]){
-                maxArea = max(maxArea,(e-s) * height[s]);
-                s++;
+        int index1 = 0; int index2 = height.size()-1;
+        while(index1<index2){
+            int distance = index2-index1;
+            if(height[index1]>height[index2]){
+                
+                maxArea = max(maxArea,height[index2]*distance);
+                index2--;
             }else{
-                maxArea = max(maxArea,(e-s) * height[e]);
-                e--;
+                maxArea = max(maxArea,height[index1]*distance);
+                index1++;
             }
         }
         return maxArea;
